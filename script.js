10e9c905f5bcf6b924a4e904e42c0440daa886e6
Megenvillota <Megenvillota@yahoo.com>
2022-01-16 14:37:59 -0500

    changes

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-let weather = {
-  apiKey: "9c4faf716efed4577c8b69314d3524d2",
-  fetchWeather: function (city) {
-    fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&units=imperial&appid=" +
-        this.apiKey
-    )
-      .then((response) => response.json())
-      .then((data) => displayWeather(data));
-  },
-  displayWeather: function (data) {},
-};
+function fetchWeather() {
+  var city = document.getElementById("city").value;
+  var apiKey = "8ac688db61c6f5e6e946317d176fb5c2";
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&units=imperial&appid=" +
+      apiKey
+  )
+    .then((response) => response.json())
+    .then((data) => this.displayWeather(data));
+}
+function displayWeather(data) {
+  const { name } = data;
+  const { temp } = data.main;
+  const { description } = data.weather[0];
+
+  var des = String(description);
+  console.log(name, description, temp);
+  document.querySelector(".city").innerHTML = "Weather in " + name;
+  document.querySelector(".temp").innerHTML = temp + "°F";
+  document.querySelector(".description").innerHTML = description;
+}
